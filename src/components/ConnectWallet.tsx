196aa0d4b4cb6a389b93f2c5c9c5f03ff825e3b5
MacePapa <83501271+MacePapaCrypto@users.noreply.github.com>
2022-02-15 23:21:42 -0500

    Update to frontend
    Added server calls from frontend to backend

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -2,6 +2,8 @@ import { useContext, useEffect } from 'react';
 import { Context } from '../Store';
 import { checkNetwork } from '../functions/ethersFunctions';
 import formatAddress from '../Reducer';
+import { ethers, Wallet } from 'ethers';
+import axios from 'axios';
 
 declare const window:any;
 
@@ -21,15 +23,65 @@ export default function ConnectWallet() {
     }
 
     const foundAddress = await window.ethereum.request({ method: "eth_requestAccounts" });
-    console.log(foundAddress);
+    //console.log(foundAddress);
 
     if (foundAddress[0] !== undefined){
       dispatch({ type:'walletAddress', content: foundAddress[0] });
     }
   }
 
+  const params = new URLSearchParams(window.location.search);
+  if (!params.has("code")) {
+    alert("The link you have used is invalid or expired");
+    // TODO: Make the page look disabled
+  }
+  const code = params.get("code");
+  // TODO: Verify Code Here (on page load)
+
   const signMessageToConnect = async () => {
-    
+    try {
+      //Check if the code is valid/expired and code is already used and user is verified
+      let { data } = await axios.post("/.netlify/functions/checkCode", {
+        code,
+      });
+
+      if (!data.verified) {
+        alert("Link is invalid. Please try verifying again on discord.");
+        return;
+      }
+
+      // Get the user account
+      const accounts = await window.ethereum.enable();
+      const account = accounts[0];
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      // The Metamask plugin also allows signing transactions to
+      // send ether and pay to change state within the blockchain.
+      // For this, you need the account signer...
+      const signer = provider.getSigner();
+      const message = await signer.signMessage(code!.toString());
+      data = await axios.post("/.netlify/functions/verify", {
+        message,
+        code,
+        account,
+      });
+      console.log(data);
+      console.log(account);
+      console.log(provider);
+
+      if (data.data.verified) {
+        setTimeout(() => {
+          alert("Verification successful! You can safely close this page.");
+        }, 30);
+      } else {
+        alert(
+          "Verification was unsuccessful. Please try verifying again on discord."
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Something went wrong, Please try again in a few moments.");
+    }
   }
 
   const connectWalletAndSign = async () => {
@@ -39,7 +91,7 @@ export default function ConnectWallet() {
 
   return (
     <div className="connectInfo">
-      <a onClick={connectWalletFunction} className="connectButton">{ state.walletAddress === "" ? "Connect" : "Connected" }</a>
+      <a role="button" onClick={connectWalletAndSign} className="connectButton">{ state.walletAddress === "" ? "Connect" : "Connected" }</a>
     </div>
   );
 }
---
+++ src/functions/verify.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+    const response = await fetch(`${process.env.SERVER_ADDRESS}/verifyAddress`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: event.body
+    })
+
+    // above this is the front end requesting the express server to check stuff or whatever
+
+    // below this is the response from the express server
+    const json = await response.json()
+
+    return {
+        body: JSON.stringify(json),
+        statusCode: 200,
+    };
+}
